3430e436b907656493f17f40ff20b3e6deb03b01
Deepak jotwani <44091069+deepakjotwani@users.noreply.github.com>
2020-10-27 12:48:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello Sample1 Repo'
+    return 'Hello Sample1!'
 
 @app.errorhandler(500)
 def server_error(e):
